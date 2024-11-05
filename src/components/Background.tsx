ef3cd700f2230d912a53e42d094f57673a4dd387
iMalFect <me@imal.dev>
2024-11-05 23:08:13 +0100

    i really am going to build it locally next time 💀

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { motion, useScroll, useTransform } from 'framer-motion';
 import { useMemo } from 'react';
 
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,5 @@
 'use client';
 // This component basically houses the entire website, and is used so that the metadata can be used in the page.
-import Background from '@/components/Background';
 import Footer from '@/components/Footer';
 import HeroSection from '@/sections/Hero';
 import IntroductionSection from '@/sections/Introduction';
@@ -10,6 +9,10 @@ import SocialsSection from '@/sections/Socials';
 import TeamSection from '@/sections/Team';
 import TradeSection from '@/sections/Trade';
 import VideosSection from '@/sections/Videos';
+import dynamic from 'next/dynamic';
+const Background = dynamic(() => import('@/components/Background'), {
+	ssr: false
+});
 
 export default function Home() {
 	return (
