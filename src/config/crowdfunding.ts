70a1e287ffd292197330e95e0705eb578bde1163
argonmining <ashton@onargon.com>
2024-12-16 14:25:21 -0800

    campaign closed

--- src/config/crowdfunding.ts
+++ src/config/crowdfunding.ts
@@ -4,14 +4,14 @@ const crowdfunding: {
 	active: boolean;
 	campaign?: CrowdfundingCampaign;
 } = {
-	active: true,
+	active: false,
 	campaign: {
 		title: 'Major Exchange Listing Campaign',
 		goal: 50000,
 		address: 'kaspa:qzrsq2mfj9sf7uye3u5q7juejzlr0axk5jz9fpg4vqe76erdyvxxze84k9nk7',
 		adjustment: {
 			kas: '0',
-			nacho: '8587910057895573'
+			nacho: '0'
 		}
 	}
 };
