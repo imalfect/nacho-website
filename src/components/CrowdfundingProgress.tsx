b75d4cf90c4db5cd07801390502a3699fc14c5a8
iMalFect <me@imal.dev>
2024-12-16 12:18:19 +0100

    reformat and make button smaller

--- src/components/CrowdfundingProgress.tsx
+++ src/components/CrowdfundingProgress.tsx
@@ -1,3 +1,5 @@
+import DonateDialog from '@/components/Dialogs/DonateDialog';
+import { MediumButton } from '@/components/ui/Buttons/MediumButton';
 import { Progress } from '@/components/ui/progress';
 import useAdjustedBalances from '@/hooks/useAdjustedBalances';
 import useKRC20Balance from '@/hooks/useKRC20Balance';
@@ -6,11 +8,9 @@ import useTokenPrice from '@/hooks/useTokenPrice';
 import { CrowdfundingCampaign } from '@/types/crowdfundingCampaign';
 import calculateTokenValue from '@/utils/calculateTokenValue';
 import shiftDecimals from '@/utils/shiftDecimals';
-import { useEffect, useState } from 'react';
 import { useAnimate } from 'framer-motion';
-import { PartButton } from '@/components/ui/Buttons/PartButton';
 import { LucideCoins } from 'lucide-react';
-import DonateDialog from '@/components/Dialogs/DonateDialog';
+import { useEffect, useState } from 'react';
 
 export default function CrowdfundingProgress(props: { campaign: CrowdfundingCampaign }) {
 	const nachoPrice = useTokenPrice('nacho-the-kat');
@@ -41,7 +41,6 @@ export default function CrowdfundingProgress(props: { campaign: CrowdfundingCamp
 		setNachoValue(calculateTokenValue(shiftDecimals(adjustedValues.nacho, -8, 2), nachoPrice));
 		setKaspaValue(calculateTokenValue(shiftDecimals(adjustedValues.kaspa, -8, 2), kaspaPrice));
 	}, [adjustedValues, kaspaPrice, nachoPrice, setKaspaValue, setNachoValue]);
-
 	useEffect(() => {
 		const delay = 0.5;
 		animateProgress('.progress-title', { opacity: 1 }, { delay }).then(() => {
@@ -57,25 +56,24 @@ export default function CrowdfundingProgress(props: { campaign: CrowdfundingCamp
 
 	return (
 		<div className={'flex flex-col gap-3'} ref={progressScope}>
-			<h2 className={'text-3xl font-bold progress-title opacity-0'}>{props.campaign.title}</h2>
+			<h2 className={'progress-title text-3xl font-bold opacity-0'}>{props.campaign.title}</h2>
 			<div className={'progress-bar opacity-0'}>
 				<Progress
 					value={Math.min((totalRaisedUSD / props.campaign.goal) * 100, 100)}
 					className={'max-w-[48rem]'}
 				/>
 			</div>
-			<p className={'text-lg progress-text opacity-0'}>
-				Raised: {totalRaisedUSD.toLocaleString('en-US', { style: 'currency', currency: 'USD' })} / 
-					Goal: {props.campaign.goal.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}
+			<p className={'progress-text text-lg opacity-0'}>
+				Raised: {totalRaisedUSD.toLocaleString('en-US', { style: 'currency', currency: 'USD' })} / Goal:{' '}
+				{props.campaign.goal.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}
 			</p>
 			<div className={'mb-10 flex flex-wrap gap-6'}>
-				<PartButton
-					className={'donate-button opacity-0 inline-block'}
+				<MediumButton
+					className={'donate-button inline-block opacity-0'}
 					onClick={() => setShowDonateDialog(true)}
-					icon={<LucideCoins />}
 				>
-					Donate
-				</PartButton>
+					<LucideCoins /> Donate
+				</MediumButton>
 			</div>
 			<DonateDialog show={showDonateDialog} onClose={() => setShowDonateDialog(false)} />
 		</div>
